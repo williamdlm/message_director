ae4513aa559c11278be525be295ba878711ed204
William Mota <william.mota.ext@spms.min-saude.pt>
2024-06-15 20:26:28 +0100

    Initial commit

---
+++ src/main/java/com/github/williamdlm/message_director/MessageDirectorApplication.java
@@ -0,0 +1,13 @@
+package com.github.williamdlm.message_director;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageDirectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageDirectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/williamdlm/message_director/MessageDirectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.williamdlm.message_director;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageDirectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
