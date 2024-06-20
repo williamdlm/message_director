f1915803a89c3f423c924be7e50c4b9d931ca7eb
William Mota <william.mota.ext@spms.min-saude.pt>
2024-06-20 15:21:21 +0100

    feat: Added generic parser to HL7v2

---
+++ src/main/java/com/github/williamdlm/message_director/pojo/hl7v2/ParserHL7v2.java
@@ -0,0 +1,28 @@
+package com.github.williamdlm.message_director.pojo.hl7v2;
+
+import ca.uhn.hl7v2.DefaultHapiContext;
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.HapiContext;
+import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.parser.CanonicalModelClassFactory;
+import ca.uhn.hl7v2.parser.PipeParser;
+import com.github.williamdlm.message_director.enums.DataFormat;
+import com.github.williamdlm.message_director.exception.FileTypeNotFound;
+import com.github.williamdlm.message_director.pojo.Utils;
+
+public class ParserHL7v2 {
+
+    public static Message toParser(String resource) throws HL7Exception {
+//        HapiContext context = new DefaultHapiContext();
+//        CanonicalModelClassFactory mcf = new CanonicalModelClassFactory("2.5");
+//        context.setModelClassFactory(mcf);
+
+        Message message = null;
+        if (Utils.findType(resource).equals(DataFormat.HL7_PIPE_ENCODING)) {
+            PipeParser pipeParser = new PipeParser();
+            message = pipeParser.parse(resource);
+            return message;
+        }
+        throw new FileTypeNotFound();
+    }
+}
