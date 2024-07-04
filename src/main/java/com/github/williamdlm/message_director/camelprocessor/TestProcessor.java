37774fe8b887a35154442454997cb310ff377c21
William Mota <william.mota.ext@spms.min-saude.pt>
2024-07-04 13:00:20 +0100

    refactor: add annotation component in TestProcessor

--- src/main/java/com/github/williamdlm/message_director/camelprocessor/TestProcessor.java
+++ src/main/java/com/github/williamdlm/message_director/camelprocessor/TestProcessor.java
@@ -2,7 +2,9 @@ package com.github.williamdlm.message_director.camelprocessor;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TestProcessor implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
